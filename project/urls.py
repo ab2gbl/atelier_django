7851f3419dbb87d430917fe42858b20f3baf8c33
ab2gbl <118647191+ab2gbl@users.noreply.github.com>
2023-08-24 17:48:33 +0000

    document 1

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 
+from django.shortcuts import redirect
 
 
 from django.urls import re_path
@@ -38,12 +39,13 @@ urlpatterns = [
     
     
     #documentation
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
     path('schema/',SpectacularAPIView.as_view(),name="schema"),
     path('schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
+    path('',lambda request: redirect('schema/docs/'),)
     
    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
